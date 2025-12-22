18e58f3fa91964cb4d580fe463535d43146a9d9f
Ahmed-Samir12 <ahmed.samir27901@gmail.com>
2025-12-22 02:25:50 +0200

    Added error handling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import helmet from 'helmet';
 import express from 'express';
 import morgan from 'morgan';
+import AppError from './utils/AppError.js';
+import globalErrorHandler from './controllers/errorController.js';
 
 const app = express();
 
@@ -19,5 +21,13 @@ app.use(
 app.get('/api/v1/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
+
+// 404 handler for undefined routes
+app.all('/{*splat}', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+
+// Global error middleware
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/controllers/errorController.js
@@ -0,0 +1,40 @@
+// For sending error responses in development environment
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+// For sending error responses in production environment
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or unknown error
+  else {
+    console.error('💥 ERROR:', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+export default (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    sendErrorProd(err, res);
+  }
+};
